1cab88d689f9ef1e5258eedefda30bca18196686
JohnH2034 <jrhoffm@yahoo.com>
2025-08-05 18:55:24 -0600

    changed ssh to variable

--- create-cloud-init.sh
+++ create-cloud-init.sh
@@ -3,7 +3,7 @@
 VM_NAME=$1
 IP_ADDR=$2
 
-#PUB_KEY=$(cat /home/ansible/.ssh/id_rsa.pub)
+PUB_KEY=$(cat /home/ansible/.ssh/id_rsa.pub)
 
 VM_DIR="/tmp/cloud-init/${VM_NAME}"
 
@@ -23,7 +23,7 @@ users:
     gecos: Ansible User
     sudo: ALL=(ALL) NOPASSWD:ALL
     ssh_authorized_keys:
-      - ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDAGaQ8oNCWHqLwAdwYkxF1aJxvZO0HMV+4nXFtCkPKGUDXS3YPwz5J4vZSYKWtTZ/6jcKiyIQ9VQUZbP/WeetD+5hNVy6riwZPPyBrZUZvXJzZJJqA5FgT0H99373gOIPd7isi4gjJOXr67OzzeaTAPushbjJC/F9jaknwaf2fbvhyExD86fiTSB70Jtp4DTD0CNtHK+ja41HJubH5QBIWw9mF7xOokh8I+IDrHDZKJ6hftt3ZrHvylHElc+HVUf41c1CR1HN30xwfh8HkP4xpF4CoMc6VbfZL6fef/jgKTpf0gVomdSJchigIxJ7DbJ5YVXwqaPIuOPicVXh2i+KPq1YDMQh2h1ZnJjM57pzNmfShpRuRZnA7BXTQMOolsGCylTV1eHI10OYg+xWSbtUBzLzqA3q2V7KnWNOMkM6UuD2fsSEwug7NA8vZAGa2shZKBMv4WwKeqxw1NadzMgW3AZeWPWtdtEHjE3DCodpbzwFtmzwfwBHVfClcm1S7wYQxNAzIWGwsINdrNBjS4YbZdqJLs9AaAZ6yrE9sgUPlgZ220zrEkvhvooEbkqXfZ5/78l8gOM5CmsRsJuhusoz2mc2LhdKHSS81yqXp+g3QY/u7+WSVgvdJXFGXb8A6KybzGjtU6agETbf669f9zW8E2u8dqZ1ztPoXPG4Rrnsmtw== ansible@jhtest
+      - "ssh-rsa ${PUB_KEY}"
 
 disable_root: false
 ssh_pwath:  true
